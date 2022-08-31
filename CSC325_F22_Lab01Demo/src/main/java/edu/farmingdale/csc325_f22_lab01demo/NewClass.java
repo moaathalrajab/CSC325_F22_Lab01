5402d95caefea98fdab80004ba53ef542d82b825
alrajam@farmingdale.edu <soblab@WHIT208W10_00.it.farmingdale.edu>
2022-08-31 10:27:55 -0400

    Add sayHello with iterator

--- CSC325_F22_Lab01Demo/src/main/java/edu/farmingdale/csc325_f22_lab01demo/NewClass.java
+++ CSC325_F22_Lab01Demo/src/main/java/edu/farmingdale/csc325_f22_lab01demo/NewClass.java
@@ -1,13 +1,33 @@
 package edu.farmingdale.csc325_f22_lab01demo;
-
 public class NewClass {
     
     private static void sayHello(String str){
         System.out.println(str);
     }
     
+    private static void sayHello(String str, int n){
+        int i=0;
+       
+        do{
+            i++;
+            System.out.println(str);
+        }while(i<n);
+    }
+     
     public static void main(String[] args) {
-        sayHello("Hello World");
+        sayHello("Hello World",5);
+    }  
+}
+
+class Class2{
+    public Class2(){
+        
+    }
+    public Class2(int x){
+        
+    }
+    public Class2(String x){
+        
     }
     
 }
