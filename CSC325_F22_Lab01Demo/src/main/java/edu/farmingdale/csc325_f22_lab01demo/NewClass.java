a5b97f89d3f94b3b7296b926d9e3e1fa2a009d37
alrajam@farmingdale.edu <soblab@WHIT208W10_00.it.farmingdale.edu>
2022-08-31 09:58:20 -0400

    Add main method and print a "hello world"

--- CSC325_F22_Lab01Demo/src/main/java/edu/farmingdale/csc325_f22_lab01demo/NewClass.java
+++ CSC325_F22_Lab01Demo/src/main/java/edu/farmingdale/csc325_f22_lab01demo/NewClass.java
@@ -2,4 +2,8 @@ package edu.farmingdale.csc325_f22_lab01demo;
 
 public class NewClass {
     
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
 }
