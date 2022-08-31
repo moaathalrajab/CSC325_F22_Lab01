e31263481141fba4d9dfb44a975e4ac7eb421e7a
alrajam@farmingdale.edu <soblab@WHIT208W10_00.it.farmingdale.edu>
2022-08-31 10:05:19 -0400

    Refactor main method and Add sayHello

--- CSC325_F22_Lab01Demo/src/main/java/edu/farmingdale/csc325_f22_lab01demo/NewClass.java
+++ CSC325_F22_Lab01Demo/src/main/java/edu/farmingdale/csc325_f22_lab01demo/NewClass.java
@@ -2,8 +2,12 @@ package edu.farmingdale.csc325_f22_lab01demo;
 
 public class NewClass {
     
+    private static void sayHello(String str){
+        System.out.println(str);
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        sayHello("Hello World");
     }
     
 }
